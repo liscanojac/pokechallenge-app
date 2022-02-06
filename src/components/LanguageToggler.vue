0d2e0b13dc7d48219e9ecf3e89b21caf94f6d5c8
Juan Canelon <liscanojac@gmail.com>
2022-02-06 20:30:07 -0300

    feat: languageToggler styles

--- src/components/LanguageToggler.vue
+++ src/components/LanguageToggler.vue
@@ -1,14 +1,14 @@
 <template>
   <label for="toggle-example" class="flex items-center cursor-pointer relative mb-4">
     <span class="mr-3 text-gray-900 text-sm font-medium">EN</span>
-    <input @click="changeLanguage()" type="checkbox" id="toggle-example" class="sr-only">
-    <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-sm"></div>
+    <input :checked="getLanguage === 'es'" @click="changeLanguage()" type="checkbox" id="toggle-example" class="sr-only">
+    <div class="toggle-bg bg-emerald-200 border-2 border-gray-200 h-6 w-11 rounded-sm"></div>
     <span class="ml-3 text-gray-900 text-sm font-medium">ESP</span>
   </label>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 
 export default {
@@ -16,21 +16,22 @@ export default {
   methods: {
     ...mapActions(["changeLanguage"]),
   },
+  computed: mapGetters(["getLanguage"]),
 }
 </script>
 
 <style scoped>
 .toggle-bg:after {
     content: '';
-    @apply absolute top-0.5 left-7 bg-white border border-gray-300 rounded-sm h-5 w-5 transition shadow-sm;
+    @apply absolute top-0.5 left-7 bg-orange-300 border border-orange-300 rounded-sm h-5 w-5 transition shadow-sm;
 }
 
 input:checked + .toggle-bg:after {
     transform: translateX(100%);
-    @apply border-white;
+    @apply border-orange-300;
 }
 
 input:checked + .toggle-bg {
-    @apply bg-blue-600 border-blue-600;
+    @apply bg-emerald-600 border-emerald-600;
 }
 </style>
